a7e30cae06b11a0d158e0e2be63cbec8bc42fab3
Hayden <hshel90@gmail.com>
2014-08-30 18:49:15 -0600

    finishing asg1

--- binaryOps.c
+++ binaryOps.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
+#include <limits.h>
+
+
+
+
+
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = b[i] & (1<<j);
+            byte >>= j;
+            printf("%u", byte);
+        }
+    }
+    puts("");
+}
 
 
 
 
 int main ()
 {
-	
-	printInt(3);
-	
-	int x = 34; 
-	printInt(x 
-	
-	
+
 	
 	return 0;
-	
-	
+
 }
 
-
-
-void printInt(int input)
+int int_shifts_are_arithmetic()
 {
 	
+		//max is largest possible integer (111111...)
+	int max = ~0;
+	int shifted = (max)>>1;
+	//bitwise XOR shifted with max
 	
-	printf("%d \n", input);	
-	
+	int res = shifted ^ max;
+	//if the machine uses arithmetic right shifts, then all the bits in res will be zero
+	return !res &&1;
+
 }
