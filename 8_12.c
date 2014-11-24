da43e846e3dbe611451f3ea2832c1a114ad72593
Hayden <hshel90@gmail.com>
2014-11-23 18:10:05 -0700

    PS10 test files

---
+++ 8_12.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+void doit()
+{
+    fork();
+    fork();
+    printf("hello\n");
+    return;
+}
+
+void doit2()
+{
+    if(fork()==0)
+    {
+    fork();
+    printf("hello \n");
+    exit(0);
+
+    }
+    return;
+
+}
+int main()
+{
+    printf("8.12 \n")
+    doit();
+    printf("hello\n");
+    printf("\n");
+
+    printf("8.14 \n");
+    doit2();
+    printf("hello \n");
+    printf("\n")
+
+    exit(0);
+}
